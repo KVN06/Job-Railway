277b749635938ff8dce4a27315948b87c6d5611e
victorcruz <velazcocruzv9@gmail.com>
2025-10-14 10:45:35 -0500

    Configurado SendGrid API

--- config/mail.php
+++ config/mail.php
@@ -32,6 +32,11 @@ return [
             'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url(env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
         ],
 
+        'sendgrid' => [
+        'transport' => 'sendgrid',
+    ],
+
+
         'ses' => [
             'transport' => 'ses',
         ],
@@ -92,8 +97,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'velazcocruzv9@gmail.com'),
+        'name' => env('MAIL_FROM_NAME', 'JobRailway'),
     ],
 
 ];
